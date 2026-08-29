bba07c5eed00c3f8d5f8b651d02de7220fd30bda
Cheezestyck <carterdcrum@gmail.com>
2026-08-29 03:20:03 -0500

    Load game fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
+import './game-fixes.css'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
