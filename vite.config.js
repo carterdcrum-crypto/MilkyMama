c4ee40c1c6609363e19b82a93caf0a572aa13485
Cheezestyck <carterdcrum@gmail.com>
2026-08-29 04:16:16 -0500

    Support native Capacitor builds

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/MilkyMama/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'native' ? './' : '/MilkyMama/',
   plugins: [react()],
   server: { host: '0.0.0.0' },
-})
+}))
